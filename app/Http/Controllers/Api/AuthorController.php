527652b27b99c366df53a2cb2456d310d437d96d
FreshDev <billyhans90@gmail.com>
2021-12-10 04:11:03 +0000

    Create Api Controllers

---
+++ app/Http/Controllers/Api/AuthorController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+   // REGISTER METOD -POST
+   public function register(Request $request) {
+
+   }
+
+   //LOGIN MEHTOD -POST
+   public function login(Request $request) {
+
+   }
+
+   //PROFILE METHOD -GET
+   public function profile() {
+
+   }
+
+   //LOGOUT METHOD -GET
+   public function logout() {
+       
+   }
+}
---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    // CREATE METHOD - POST
+    public function createBook(Request $request) {
+
+    }
+
+    // LIST METHOD -GET
+    public function listBooks() {
+
+    }
+
+    // SINGLE BOOK METHOD -GET
+    public function singleBook($book_id) {
+
+    }
+
+    // UPDATE METHOD -POST 
+    public function updateBook(Request $request, $book_id) {
+
+    }
+
+    // DELETE METHOD -GET
+    public function deleteBook($book_id) {
+        
+    }
+}
