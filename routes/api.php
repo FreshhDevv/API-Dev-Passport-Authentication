888be715a8f0271d3e17a10e77e0f8ef30db6b83
FreshDev <billyhans90@gmail.com>
2021-12-10 04:36:49 +0000

    Create API Routes & Passport Route Grouping

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\AuthorController;
+use App\Http\Controllers\Api\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,6 +16,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::post('register', [AuthorController::class, 'register']);
+Route::post('login', [AuthorController::class, 'login']);
+
+Route::group(['middleware' => ['auth:api']], function() {
+
+    Route::get('profile', [AuthorController::class, 'profile']);
+    Route::get('logout', [AuthorController::class, 'logout']);
+
+    Route::post('create/book', [BookController::class, 'createBook']);
+    Route::get('list/books', [BookController::class, 'listBooks']);
+    Route::get('single/book/{id}', [BookController::class, 'singleBook']);
+    Route::post('update/book/{id}', [BookController::class, 'updateBook']);
+    Route::get('delete/book/{id}', [BookController::class, 'deleteBook']);
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
